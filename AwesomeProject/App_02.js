db3bd242c6e2501552c87be00e1094019b5094e2
wangtapplication@outlook.com <wangtapplication@outlook.com>
2020-06-21 10:54:29 +0800

    test2 component

--- AwesomeProject/App_02.js
+++ AwesomeProject/App_02.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
 import {View ,Text} from 'react-native'
 
-const App = () => {
+const App = () =>{
     return (
         <View>
             <View>
                 <Text> Code</Text>
             </View>
-            {/* // 设置外边框，内边框，颜色 */}
-            
+            // 设置外边框，内边框，颜色
             <View style = {{marginTop:8,padding:8,backgroundColor:'blue'}}>
                 <Text style={{color: 'white'}}>code test</Text>
             </View>
-            <View style = {{marginTop:8,padding:8,width: 200, backgroundColor:'red'}}>
+            <View style = {{marginTop:8,padding:8,backgroundColor:'blue'}}>
                 <Text style={{color: 'white'}}>code test</Text>
             </View>
         </View>
